a415577fa81677acf7ee592c758d43133d01603d
Diogo Henrique <diogjp10@gmail.com>
2016-07-29 16:14:20 +0100

    Add files via upload

---
+++ MySingleton.java
@@ -0,0 +1,51 @@
+package ie.ienquire.fff.dreamteam.util;
+
+
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+
+/**
+ * Created by Diogo on 29/07/2016.
+ * DreamTeam
+ */
+public class MySingleton {
+
+    private static MySingleton instance;
+    private MySingleton(){}
+
+    public static MySingleton getInstance(){
+
+        if(instance == null)
+        {
+            initInstance();
+        }
+        return instance;
+    }
+
+    private static synchronized void initInstance()
+    {
+        if (instance == null)
+        {
+            instance = new MySingleton();
+        }
+    }
+
+    public void showToast(String s) {
+        Toast.makeText(MyApp.get(), s, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Default view animation
+     *
+     * @param view - some button, image, etc.
+     * @param anim - Ex: R.anim.fade_in
+     */
+    public void animView(View view, int anim){
+        Animation animation = AnimationUtils.loadAnimation(MyApp.get(),anim);
+        view.setAnimation(animation);
+    }
+
+
+}
